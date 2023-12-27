957e97f5f392a9efa70033c950359fbb40917ba0
PretendMask <48581481+PretendMask@users.noreply.github.com>
2023-12-27 21:12:29 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          YuLF
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          风拂碧林鱼悠悠.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
