17d7a84cedd4c242a58c98738647ecf5e893936e
PretendMask <48581481+PretendMask@users.noreply.github.com>
2023-12-27 21:28:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "YuLF",
+  description: "风萧萧兮易水寒",
   viewport: {
     width: "device-width",
     initialScale: 1,
